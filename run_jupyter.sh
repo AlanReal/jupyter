0935f19773d9795ff850c93ab133fcf9c8cd900d
alan <alanrgln@gmail.com>
2020-02-01 01:23:51 -0200

    remoção de imagens none

--- run_jupyter.sh
+++ run_jupyter.sh
@@ -5,3 +5,4 @@ docker build -t $NAME:1.0 .
 docker stop $NAME || true
 docker rm -f $NAME || true
 docker run -d --name $NAME --restart always -e PYTHONUNBUFFERED=0 -e TZ=America/Sao_Paulo -p 9999:9999 $NAME:1.0
+docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
